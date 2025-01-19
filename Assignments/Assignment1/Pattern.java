314c7bd189b94b595d3a80e10e1520ee3f88f969
Sunny <83127107+RajSunny2011@users.noreply.github.com>
2025-01-19 08:53:36 +0530

    Assignment1 added

--- Assignments/Assignment1/Pattern.java
+++ Assignments/Assignment1/Pattern.java
@@ -1,8 +1,11 @@
 public class Pattern {
     public static void main(String[] args) {
         for (int i = 5; i > 0; i--){
-            for (int j = 1; j <= i; j++){
-                System.out.print(j+" ");
+            for (int j = 5; j >= i; j--){
+                System.out.print(" ");
+            }
+            for (int k = 1; k<=i; k++){
+                System.out.print("*");
             }
             System.out.print("\n");
         }
